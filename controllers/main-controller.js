116cd5bd6d77a21c0a588c0fc7f7fad96440cdff
Sagar Katore <sagarvk90@gmail.com>
2023-07-30 17:23:29 +0530

    pdf creation completed only letterhead and footer text pending

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -3,12 +3,15 @@ import Desp from "../models/Desp.js";
 import Client from "../models/Client.js";
 import { jsPDF } from "jspdf";
 import { applyPlugin } from "jspdf-autotable";
-applyPlugin(jsPDF);
+import DOMPurify from "dompurify";
+import autoTable from "jspdf-autotable";
 import "jspdf-autotable";
 // import autoTable from "jspdf-autotable";
 import path from "path";
 import { dirname } from "path";
 import ejs from "ejs";
+applyPlugin(jsPDF);
+
 let contigency = 2.5,
   electrification = 2.5;
 
@@ -19,7 +22,7 @@ export const getAmt = async (req, res, next) => {
     plotLength,
     plotWidth,
     totalBuiltupArea,
-    floors,
+    numOfFloors,
     projectType,
     constructionQuality,
     mobileNo,
@@ -29,6 +32,8 @@ export const getAmt = async (req, res, next) => {
   let estimatecalc = [];
   let desptable = [];
   let barea = parseInt(totalBuiltupArea);
+  let bplotlength = parseInt(plotLength);
+  let bplotwidht = parseInt(plotWidth);
   try {
     qData = await Quality.findOne({ name: constructionQuality });
   } catch (err) {
@@ -69,24 +74,32 @@ export const getAmt = async (req, res, next) => {
       // desptable.push(estimatecalc)
       estimatecalc.push(
         {
-          "sr.no": sno + 1,
+          "sr.no": "",
           desp: `Add for Contengencies & Water Charges at ${contigency.toFixed(
             2
           )}%`,
           qty: "",
           unit: "",
           rate: "",
-          amount: camount,
+          amount: camount.toFixed(2),
         },
         {
-          "sr.no": sno + 2,
+          "sr.no": "",
           desp: `Add for Electrification & Other Charges at ${electrification.toFixed(
             2
           )}%`,
           qty: "",
           unit: "",
           rate: "",
-          amount: eamount,
+          amount: eamount.toFixed(2),
+        },
+        {
+          "sr.no": "",
+          desp: `TOTAL`,
+          qty: "",
+          unit: "",
+          rate: "",
+          amount: tamt.toFixed(2),
         }
       );
 
@@ -98,7 +111,7 @@ export const getAmt = async (req, res, next) => {
           length: plotLength,
           width: plotWidth,
           btarea: barea,
-          floor: floors,
+          floor: numOfFloors,
           prtype: projectType,
           conq: constructionQuality,
           mob: mobileNo,
@@ -109,6 +122,11 @@ export const getAmt = async (req, res, next) => {
       res.status(200).json({ success: true, data: estimatecalc });
 
       let edata = [];
+      let firmname = "VKON CONSULTANTS";
+      let firmadd1 = "At : Nimgaon Korhale, Post : Laxmiwadi, Tal : Rahata,";
+      let firmadd2 = "Ahmednagar - 423 109";
+      let firmcontact =
+        "Mob - +91- 9960 867 555, 9859 121 121 | Email - abc@gmail.com";
 
       estimatecalc.forEach((element, index, array) => {
         edata.push([
@@ -122,15 +140,151 @@ export const getAmt = async (req, res, next) => {
       });
       console.log[edata];
       const doc = new jsPDF("p", "in", "a4");
-      // doc.setFontSize(20);
-      // doc.text("ESTIMATE", 2.5, 1.5);
-      // doc.text("PROJECT", 2.5, 3.5);
-      // doc.fromHTML("#section1", 0.5, 0.5);
+      let pgwidth = doc.internal.pageSize.getWidth();
+      let pgheight = doc.internal.pageSize.getHeight();
 
-      autoTable(doc, {
+      let est = `<html><div><u><b>ESTIMATE</b></u></div></html>`;
+
+      // doc.html(est, {
+      //   callback: function (doc) {
+      //     // Save the PDF
+      //     console.log(est);
+      //   },
+      //   x: pgwidth / 2,
+      //   y: 1.5,
+      //   width: 8.27, //target width in the PDF document
+      //   windowWidth: 8.27, //window width in CSS pixels
+      // });
+      // doc.text("<u><b>ESTIMATE</b></u>", pgwidth / 2, 1.5, "center");
+      doc.setFontSize(20);
+      doc.setFont("Helvetica", "bold");
+      doc.text("ESTIMATE", pgwidth / 2, 1.5, "center");
+      doc.setFontSize(15);
+      doc.setFont("Helvetica", "bold");
+      doc.text("PROJECT", pgwidth / 2, 3.5, "center");
+      doc.setFont("Helvetica", "normal");
+      doc.text(
+        "PROPOSED " + projectType.toUpperCase(),
+        pgwidth / 2,
+        4.25,
+        "center"
+      );
+      doc.setFont("Helvetica", "bold");
+      doc.text("IN", pgwidth / 2, 5, "center");
+      doc.setFont("Helvetica", "normal");
+      let splitadd = doc.splitTextToSize(address.toUpperCase(), 6);
+      doc.text(splitadd, pgwidth / 2, 5.5, "center");
+      doc.text("OF", pgwidth / 2, 7, "center");
+      doc.setFont("Helvetica", "bold");
+      doc.text(customerName.toUpperCase(), pgwidth / 2, 7.5, "center");
+      doc.setFontSize(20);
+      doc.setFont("Helvetica", "bold");
+      doc.text(firmname.toUpperCase(), pgwidth / 2, 10.25, "center");
+      doc.setFontSize(15);
+      doc.setFont("Helvetica", "normal");
+      doc.text(firmadd1.toUpperCase(), pgwidth / 2, 10.5, "center");
+      doc.text(firmadd2.toUpperCase(), pgwidth / 2, 10.75, "center");
+      doc.text(firmcontact.toUpperCase(), pgwidth / 2, 11.0, "center");
+      doc.setLineWidth(0.0312);
+      doc.rect(0.5, 0.5, pgwidth - 1, pgheight - 1, "S");
+      doc.setLineWidth(0.0156);
+      doc.rect(0.575, 0.575, pgwidth - 1.15, pgheight - 1.15, "S");
+
+      doc.addPage();
+
+      doc.setFontSize(15);
+      doc.setFont("Helvetica", "bold");
+      doc.line(0.5, 2.5, 7.75, 2.5);
+      doc.text("ESTIMATE SUMMARY", pgwidth / 2, 2.8, "center");
+      doc.setFontSize(11);
+      doc.setFont("Helvetica", "bold");
+      doc.line(0.5, 3, 7.75, 3);
+      doc.text("NAME OF CLIENT", 0.75, 3.25);
+      let splitcname = doc.splitTextToSize(customerName.toUpperCase(), 4);
+      doc.text(splitcname, 3.25, 3.25);
+      doc.line(0.5, 3.5, 7.75, 3.5);
+      doc.text("ADDRESS", 0.75, 3.75);
+      let splitaddress = doc.splitTextToSize(address.toUpperCase(), 4);
+      doc.text(splitaddress, 3.25, 3.75);
+      doc.line(0.5, 5.25, 7.75, 5.25);
+      doc.text("PLOT DIMENTION", 0.75, 5.75);
+      doc.text("L (Ft.)", 3.5, 5.5);
+      let flen = bplotlength.toFixed(2);
+      let splitlength = doc.splitTextToSize(flen, 1);
+      doc.text(splitlength, 3.5, 6);
+      doc.text("L (M)", 4.75, 5.5);
+      let mlen = bplotlength / 3.28;
+      mlen = mlen.toFixed(2);
+      let splitmlen = doc.splitTextToSize(mlen, 1);
+      doc.text(splitmlen, 4.75, 6);
+      doc.text("W (Ft.)", 5.75, 5.5);
+      let fwidth = bplotwidht.toFixed(2);
+      let splitwidth = doc.splitTextToSize(fwidth, 1);
+      doc.text(splitwidth, 5.75, 6);
+      doc.text("W (M)", 6.75, 5.5);
+      let mwid = bplotwidht / 3.28;
+      mwid = mwid.toFixed(2);
+      let splitmwid = doc.splitTextToSize(mwid, 1);
+      doc.text(splitmwid, 6.75, 6);
+      doc.line(3, 5.75, 7.75, 5.75);
+      doc.line(4.25, 5.25, 4.25, 6.25);
+      doc.line(5.5, 5.25, 5.5, 6.25);
+      doc.line(6.5, 5.25, 6.5, 6.25);
+
+      doc.line(0.5, 6.25, 7.75, 6.25);
+      doc.text("TOTAL BULILTUP AREA", 0.75, 6.75);
+      doc.text("SQ.FT.", 3.75, 6.5);
+      let farea = barea.toFixed(2);
+      let splitbarea = doc.splitTextToSize(farea, 1);
+      doc.text(splitbarea, 3.75, 7);
+      doc.line(5.25, 6.25, 5.25, 7.25);
+      doc.text("SQ.M.", 6.25, 6.5);
+      let marea = barea / 10.764;
+      marea = marea.toFixed(3);
+      let splitmarea = doc.splitTextToSize(marea, 1);
+
+      doc.text(splitmarea, 6.25, 7);
+      doc.line(3, 6.75, 7.75, 6.75);
+
+      doc.line(0.5, 7.25, 7.75, 7.25);
+      doc.text("NO. OF FLOORS", 0.75, 7.5);
+      let floornum = String(numOfFloors);
+      doc.text(floornum, 3.5, 7.5);
+      doc.line(0.5, 7.75, 7.75, 7.75);
+
+      doc.text("TOTAL ESTIMATE AMOUNT", 0.75, 8);
+      doc.text(tamt.toFixed(2), 4.25, 8);
+      doc.line(0.5, 8.25, 7.75, 8.25);
+      doc.text("APPROXIMATE RATE PER SQ.FT.", 0.75, 8.5);
+      let apprate = tamt / barea;
+      apprate = apprate.toFixed(2);
+      apprate = String(apprate);
+      doc.text(apprate, 4.25, 8.5);
+      doc.line(0.5, 8.75, 7.75, 8.75);
+
+      doc.line(0.5, 2.5, 0.5, 8.75);
+      doc.line(3, 3, 3, 7.75);
+      doc.line(4, 7.75, 4, 8.75);
+
+      doc.line(7.75, 2.5, 7.75, 8.75);
+
+      doc.addPage();
+
+      doc.autoTable({
+        columnStyles: { 5: { halign: "right" } },
         head: [["SR.NO.", "DESCRIPTION", "QUANTITY", "UNIT", "RATE", "AMOUNT"]],
         body: edata,
+        theme: "plain",
+        headStyles: {
+          lineWidth: 0.0104,
+          lineColor: [0, 0, 0],
+        },
+        bodyStyles: {
+          lineWidth: 0.0104,
+          lineColor: [0, 0, 0],
+        },
       });
+
       doc.save("table.pdf");
     }
   }
--- server.js
+++ server.js
@@ -7,31 +7,37 @@ import qualityrouter from "./routes/quality.route.js";
 import desprouter from "./routes/desp.route.js";
 import clientrouter from "./routes/client.route.js";
 import estimaterouter from "./routes/main.route.js";
-
-
+import ptyperouter from "./routes/ptype.route.js";
 
 // Express Route
 
 // Connecting MongoDB Database
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/estimator").then(() => app.listen(4000)).then(()=>{
-console.log('Database successfully connected!')
-},
-error => {
-	console.log('Could not connect to database : ' + error)
-})
+mongoose
+  .connect("mongodb://localhost:27017/estimator")
+  .then(() => app.listen(4000))
+  .then(
+    () => {
+      console.log("Database successfully connected!");
+    },
+    (error) => {
+      console.log("Could not connect to database : " + error);
+    }
+  );
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(cors());
-app.use("/quality",qualityrouter);
-app.use("/desp",desprouter);
-app.use("/client",clientrouter);
-app.use("/estimate",estimaterouter);
-
+app.use("/quality", qualityrouter);
+app.use("/desp", desprouter);
+app.use("/client", clientrouter);
+app.use("/estimate", estimaterouter);
+app.use("/ptype", ptyperouter);
 // app.get("/", async(req,res)=>{
 // res.json({message : "Server Runing..."})
 // })
